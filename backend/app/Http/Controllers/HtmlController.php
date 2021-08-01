c69f7fb93f722e6a174083b8f615d9f797457163
gichi939 <gichi939@gmail.com>
2021-08-01 16:25:44 +0900

    javascript fix

--- backend/app/Http/Controllers/HtmlController.php
+++ backend/app/Http/Controllers/HtmlController.php
@@ -9,7 +9,12 @@ class HtmlController extends Controller
     public function show() 
     {
         $words = array();
-        $words = ["div","h1","ul","li","a","img","class","id","section"];
+        $words = ["html","head","link","meta","style","title","body","footer",
+        "header","h1","main","nav","section","blockquote","dd","div",
+        "dl","dt","hr","li","ol","p","pre","ul","a","abbr","br","cite","code",
+        "data","i","mark","q","span","strong","time","audio","img","video","svg",
+        "canvas","script","table","td","th","thead","tr","button","form","input",
+        "label","option","select","textarea","details"];
         
         return view('html/index')->with('words',$words);
     }
--- backend/resources/views/html/index.blade.php
+++ backend/resources/views/html/index.blade.php
@@ -7,19 +7,45 @@
 @section('title', 'html_langage')
 
 @section('content')
+
+
+        
+
 <div class="container">
     <div class="row">
         <!-- left side -->
         <div class="word-list small-3 medium-4 large-3 columns">
-            @for ($i = 0; $i < count($words); $i++) <div class="word-select">
-                <li class="word" id="wordNumber{{ $i }}" data-name="$words{{ $i }}">{{ $words[$i] }}</li>
+            @for ($i = 1; $i <= count($words); $i++) <div class="word-select">
+                @php
+                $word = $words[$i-1];
+                @endphp
+                <li class="word" id="wordNumber{{ $i }}" onclick="menuButton(this, @json($i))"><span class=number>{{ $i }}</span>{{ $word }}</li>
         </div>
         @endfor
+        <script>
+            function menuButton(element,num) {
+
+        //   document.getElementById('edit_area').innerHTML = @json($words)[num-1];
+                words = [
+                    { title:"html",
+                    description:"ホームページの基礎",
+                    },
+                    { title:"head",
+                    description:"ホームページの情報",
+                    },
+                ];
+                document.getElementById('edit_area').innerHTML = words[0]["title"];
+                var wordmean = document.getElementById("txt-hide");
+                wordmean.innerHTML = words[0]["description"];
+                var prgmean = document.getElementById("prg-hide");
+                prgmean.innerHTML = '分割テキスト（左・中央・右揃え)'; 
+        }
+        </script>
     </div>
     <!-- center -->
     <div class="main-word-area offset-1 col-8">
         <div class="main-word">
-            <p id="edit_area">div</p>
+            <p id="edit_area">html</p>
         </div>
 
         <div class="word-mean">
@@ -39,6 +65,8 @@
         </div>
     </div>
 </div>
+<h2>php_jsontostring</h2>
+
 </div>
 
 
