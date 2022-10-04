1a95fab35267fcb0df67d1b450974bd4a1bb0f10
gichi939 <gichi939@gmail.com>
2022-10-04 18:49:34 +0900

    Remove the function to view examples

--- backend/resources/views/html/index.blade.php
+++ backend/resources/views/html/index.blade.php
@@ -10,16 +10,9 @@
 
 
 <div class="container">
-    <!-- center -->
+    
     <div class="row">
 
-        <div class="main-word-area col-12">
-            <div class="main-word">
-                <p id="edit_area">html</p>
-            </div>
-        </div> 
-    </div>
-    <div class="row">
         <!-- left side -->
         <div class="word-list small-3 medium-4 large-3 columns">
             @for ($i = 1; $i <= count($words); $i++) 
@@ -33,57 +26,42 @@
             @endfor
         </div>
 
-
-
-    
-        <script>
-            function menuButton(element, num) {
-
-                document.getElementById('edit_area').innerHTML = @json($words)[num - 1][0];
-                document.getElementById('txt-hide').innerHTML = @json($words)[num - 1][1];
-                document.getElementById('prg-hide').innerHTML = @json($words)[num - 1][2];
-
-            }
-        </script>
-    
-        <div class="offset-1 col-8">  
+        <!-- center -->
+        <div class="main-word-area offset-1 col-8">
+            <div class="main-word">
+                <p id="edit_area">html</p>
+            </div>
+        
+            <div class="col-12">  
             
-            <div class="word-mean">
-                <p id="more" class="more"></p>
+                <div class="word-mean">
+                    <p id="more" class="more"></p>
+                </div>
+                <div id="txt-hide">
+                    <p>ハイパーテキストマークアップ言語<br></p>
+                </div>
+        
+                <div class="word-mean">
+                    <p id="prg-more" class="prg-more"></p>
+                </div>
+        
+        
+                <div id="prg-hide">
+                    <p>HTMLの<html>要素は HTML 文書においてルート (基点) となる要素 (トップレベル要素) であり、ルート要素とも呼ばれます。他のすべての要素は、この要素の子孫として配置しなければなりません。</p>
+                </div>
+        
             </div>
-            <div id="txt-hide">
-                <p>ハイパーテキストマークアップ言語<br></p>
-            </div>
-
-            <div class="word-mean">
-                <p id="prg-more" class="prg-more"></p>
-            </div>
-
-
-            <div id="prg-hide">
-                <p>HTMLの<html>要素は HTML 文書においてルート (基点) となる要素 (トップレベル要素) であり、ルート要素とも呼ばれます。他のすべての要素は、この要素の子孫として配置しなければなりません。</p>
-            </div>
-
-            <div class="word-mean">
-                <p id="prg-example" class="prg-example"></p>
-            </div>
-
-            <div id="example-hide">
-                <pre>
-                    <code>
-                        &lthtml&gt
-                            &lthead&gt
-                                &lttitle&gt文書のタイトル&lt/title&gt
-                            &lt/head&gt
-                            &ltbody&gt
-                                &ltp&gt文書の本体&lt/p&gt
-                            &lt/body&gt
-                        &lt/html&gt
-                    </code>
-                </pre>
-            </div>
-        </div>
+        </div> 
     </div>
+        
+    <script>
+        function menuButton(element, num) {
+
+            document.getElementById('edit_area').innerHTML = @json($words)[num - 1][0];
+            document.getElementById('txt-hide').innerHTML = @json($words)[num - 1][1];
+
+        }
+    </script>
 
 </div>
 
